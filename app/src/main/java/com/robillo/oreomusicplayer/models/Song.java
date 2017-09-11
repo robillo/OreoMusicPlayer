cde93a7f9b8e4067c7802714ec3b7654ed502131
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-09-11 22:30:09 +0530

    recreated the Song model class

--- app/src/main/java/com/robillo/oreomusicplayer/models/Song.java
+++ app/src/main/java/com/robillo/oreomusicplayer/models/Song.java
@@ -10,16 +10,126 @@ public class Song implements Serializable {
 
     private String data;
     private String title;
-    private String album;
-    private String artist;
+    private String titleKey;
+    private String id;
+    private String dateAdded;
+    private String dateModified;
     private String duration;
+    private String composer;
+    private String album;
+    private String albumId;
+    private String albumKey;
+    private String artist;
+    private String artistId;
+    private String artistKey;
+    private String size;
+    private String year;
 
-    public Song(String data, String title, String album, String artist, String duration) {
+    public Song(String data, String title, String titleKey, String id, String dateAdded, String dateModified, String duration, String composer, String album, String albumId, String albumKey, String artist, String artistId, String artistKey, String size, String year) {
         this.data = data;
         this.title = title;
-        this.album = album;
-        this.artist = artist;
+        this.titleKey = titleKey;
+        this.id = id;
+        this.dateAdded = dateAdded;
+        this.dateModified = dateModified;
         this.duration = duration;
+        this.composer = composer;
+        this.album = album;
+        this.albumId = albumId;
+        this.albumKey = albumKey;
+        this.artist = artist;
+        this.artistId = artistId;
+        this.artistKey = artistKey;
+        this.size = size;
+        this.year = year;
+    }
+
+    public String getTitleKey() {
+        return titleKey;
+    }
+
+    public void setTitleKey(String titleKey) {
+        this.titleKey = titleKey;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getComposer() {
+        return composer;
+    }
+
+    public void setComposer(String composer) {
+        this.composer = composer;
+    }
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumKey() {
+        return albumKey;
+    }
+
+    public void setAlbumKey(String albumKey) {
+        this.albumKey = albumKey;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistKey() {
+        return artistKey;
+    }
+
+    public void setArtistKey(String artistKey) {
+        this.artistKey = artistKey;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
     }
 
     public String getDuration() {
--- app/src/main/java/com/robillo/oreomusicplayer/views/fragments/SongsListFragment.java
+++ app/src/main/java/com/robillo/oreomusicplayer/views/fragments/SongsListFragment.java
@@ -94,25 +94,24 @@ public class SongsListFragment extends Fragment implements LoaderManager.LoaderC
             while (cursor.moveToNext()) {
                 String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
                 String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
-                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
-                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                String titleKey = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE_KEY));
+                String id = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
+                String dateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED));
+                String dateModified = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_MODIFIED));
                 String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
-                String temp1 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.COMPOSER));
-                String temp2 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
-                String temp3 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
-                String temp4 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_KEY));
-                String temp5 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID));
-                String temp6 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_KEY));
-                String temp7 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED));
-                String temp8 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_MODIFIED));
-                String temp9 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE));
-                String temp10 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.YEAR));
-                String temp11 = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE_KEY));
-
-                Log.e("STRINGS ", data + "\n" + title + "\n" + album + "\n" + artist + "\n" + duration + "\n" + temp1 + "\n" + temp2 + "\n" + temp3 + "\n" + temp4 + "\n" + temp5 + "\n" + temp6 + "\n" + temp7 + "\n" + temp8 + "\n" + temp9 + "\n" + temp10 + "\n" + temp11);
+                String composer = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.COMPOSER));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String albumId = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
+                String albumKey = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_KEY));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                String artistId = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID));
+                String artistKey = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_KEY));
+                String size = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE));
+                String year = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.YEAR));
 
                 // Save to audioList
-                audioList.add(new Song(data, title, album, artist, duration));
+                audioList.add(new Song(data, title, titleKey, id, dateAdded, dateModified, duration, composer, album, albumId,
+                        albumKey, artist, artistId, artistKey, size, year));
             }
         }
         mAdapter = new SongsAdapter(audioList, getActivity());
