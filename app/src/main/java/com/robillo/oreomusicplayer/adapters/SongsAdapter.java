dbbfc415c5b07d48e6d1a18ebad10b39dae927c1
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-09-13 15:27:13 +0530

    Song model class implementing Parcelable

--- app/src/main/java/com/robillo/oreomusicplayer/adapters/SongsAdapter.java
+++ app/src/main/java/com/robillo/oreomusicplayer/adapters/SongsAdapter.java
@@ -1,7 +1,13 @@
 package com.robillo.oreomusicplayer.adapters;
 
+import android.content.ContentUris;
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +16,19 @@ import com.robillo.oreomusicplayer.R;
 import com.robillo.oreomusicplayer.adapters.holders.SongHolder;
 import com.robillo.oreomusicplayer.models.Song;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by robinkamboj on 11/09/17.
  */
 
-public class SongsAdapter extends RecyclerView.Adapter<SongHolder>{
+public class SongsAdapter extends RecyclerView.Adapter<SongHolder> implements View.OnClickListener{
 
     private List<Song> list;
     @SuppressWarnings("FieldCanBeLocal")
     private Context context;
+    private Uri uri;
 
     public SongsAdapter(List<Song> list, Context context) {
         this.list = list;
@@ -40,10 +48,31 @@ public class SongsAdapter extends RecyclerView.Adapter<SongHolder>{
         long mins = duration/60;
         long seconds = duration%60;
         holder.artistDuration.setText(list.get(position).getArtist() + " (" + String.valueOf(mins) + ":" + String.valueOf(seconds) + ")");
+        uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, Long.valueOf(list.get(position).getId()));
+        holder.songCard.setOnClickListener(this);
     }
 
     @Override
     public int getItemCount() {
         return list.size();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.song_card:{
+                Log.e("CLICKED", uri.toString());
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                try {
+                    mediaPlayer.setDataSource(context, uri);
+                    mediaPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mediaPlayer.start();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/robillo/oreomusicplayer/models/Song.java
+++ app/src/main/java/com/robillo/oreomusicplayer/models/Song.java
@@ -1,12 +1,15 @@
 package com.robillo.oreomusicplayer.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
 /**
  * Created by robinkamboj on 09/09/17.
  */
 
-public class Song implements Serializable {
+public class Song implements Parcelable {
 
     private String data;
     private String title;
@@ -42,6 +45,82 @@ public class Song implements Serializable {
         this.artistKey = artistKey;
         this.size = size;
         this.year = year;
+    }
+
+    /**
+     * Interface that must be implemented and provided as a public CREATOR field
+     * that generates instances of your Parcelable class from a Parcel.
+     */
+    public static final Creator<Song> CREATOR = new Creator<Song>() {
+        /**
+         * Creates a new USer object from the Parcel. This is the reason why
+         * the constructor that takes a Parcel is needed.
+         */
+        @Override
+        public Song createFromParcel(Parcel in) {
+            return new Song(in);
+        }
+
+        /**
+         * Create a new array of the Parcelable class.
+         * @return an array of the Parcelable class,
+         * with every entry initialized to null.
+         */
+        @Override
+        public Song[] newArray(int size) {
+            return new Song[size];
+        }
+    };
+
+    /**
+     * Parcel overloaded constructor required for
+     * Parcelable implementation used in the CREATOR
+     */
+    private Song(Parcel in) {
+        data = in.readString();
+        title = in.readString();
+        titleKey = in.readString();
+        id = in.readString();
+        dateAdded = in.readString();
+        dateModified = in.readString();
+        duration = in.readString();
+        composer = in.readString();
+        album = in.readString();
+        albumId = in.readString();
+        albumKey = in.readString();
+        artist = in.readString();
+        artistId = in.readString();
+        artistKey = in.readString();
+        size = in.readString();
+        year = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * This is where the parcel is performed.
+     */
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(data);
+        parcel.writeString(title);
+        parcel.writeString(titleKey);
+        parcel.writeString(id);
+        parcel.writeString(dateAdded);
+        parcel.writeString(dateModified);
+        parcel.writeString(duration);
+        parcel.writeString(composer);
+        parcel.writeString(album);
+        parcel.writeString(albumId);
+        parcel.writeString(albumKey);
+        parcel.writeString(artist);
+        parcel.writeString(artistId);
+        parcel.writeString(artistKey);
+        parcel.writeString(size);
+        parcel.writeString(year);
     }
 
     public String getTitleKey() {
