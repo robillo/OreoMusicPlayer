49dcc4eca999c70a1fe09dcf3aa6348e51fe57cf
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-03-11 23:40:37 +0530

    notification in service base

--- app/src/main/java/com/robillo/oreomusicplayer/services/MusicService.java
+++ app/src/main/java/com/robillo/oreomusicplayer/services/MusicService.java
@@ -1,10 +1,21 @@
 package com.robillo.oreomusicplayer.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Icon;
 import android.media.AudioManager;
+import android.media.MediaMetadata;
 import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.media.session.MediaSession;
+import android.media.session.MediaSessionManager;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
@@ -12,6 +23,7 @@ import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.robillo.oreomusicplayer.R;
 import com.robillo.oreomusicplayer.events.SongChangeEvent;
 import com.robillo.oreomusicplayer.models.Song;
 
@@ -27,6 +39,15 @@ public class MusicService extends Service implements
         MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
         MediaPlayer.OnCompletionListener, MusicServiceInterface {
 
+    private static final String ACTION_TOGGLE_PLAYBACK = "com.robillo.oreomusicplayer.services.TOGGLE_PLAYBACK";
+    private static final String ACTION_PREV = "com.robillo.oreomusicplayer.services.PREV";
+    private static final String ACTION_NEXT = "com.robillo.oreomusicplayer.services.NEXT";
+    public static final String ACTION_PLAY = "action_play";
+    public static final String ACTION_PAUSE = "action_pause";
+    public static final String ACTION_REWIND = "action_rewind";
+    public static final String ACTION_FAST_FORWARD = "action_fast_foward";
+    public static final String ACTION_STOP = "action_stop";
+
     private final int EMPTY_CELLS_COUNT = 2;
 
     private final IBinder musicBind = new MusicBinder();
@@ -36,6 +57,11 @@ public class MusicService extends Service implements
     private ArrayList<Song> songs;
     //current position
     private int songPosition;
+
+    //for notification
+    private MediaSessionManager mManager;
+    private MediaSession mSession;
+    private MediaController mController;
 
     @Override
     public void onCreate() {
@@ -49,6 +75,38 @@ public class MusicService extends Service implements
         player = new MediaPlayer();
 
         initMusicPlayer();
+
+        mManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
+        mSession = new MediaSession(this, "MY_SESSION");
+    }
+
+    private PendingIntent retreivePlaybackAction(int which) {
+        Intent action;
+        PendingIntent pendingIntent;
+        final ComponentName serviceName = new ComponentName(this, MusicService.class);
+        switch (which) {
+            case 1:
+                // Play and pause
+                action = new Intent(ACTION_TOGGLE_PLAYBACK);
+                action.setComponent(serviceName);
+                pendingIntent = PendingIntent.getService(this, 1, action, 0);
+                return pendingIntent;
+            case 2:
+                // Skip tracks
+                action = new Intent(ACTION_NEXT);
+                action.setComponent(serviceName);
+                pendingIntent = PendingIntent.getService(this, 2, action, 0);
+                return pendingIntent;
+            case 3:
+                // Previous tracks
+                action = new Intent(ACTION_PREV);
+                action.setComponent(serviceName);
+                pendingIntent = PendingIntent.getService(this, 3, action, 0);
+                return pendingIntent;
+            default:
+                break;
+        }
+        return null;
     }
 
     @Override
@@ -174,5 +232,72 @@ public class MusicService extends Service implements
     public void onPrepared(MediaPlayer mp) {
         //start playback
         mp.start();
+
+        //notification
+        mSession.setMetadata(new MediaMetadata.Builder()
+                .putString(MediaMetadata.METADATA_KEY_ARTIST, songs.get(songPosition).getArtist())
+                .putString(MediaMetadata.METADATA_KEY_ALBUM, songs.get(songPosition).getAlbum())
+                .putString(MediaMetadata.METADATA_KEY_TITLE, songs.get(songPosition).getTitle())
+                .build());
+        // Indicate you're ready to receive media commands
+        mSession.setActive(true);
+        // Attach a new Callback to receive MediaSession updates
+        mSession.setCallback(new MediaSession.Callback() {
+            @Override
+            public void onPlay() {
+                super.onPlay();
+                playPlayer();
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                pausePlayer();
+            }
+
+            @Override
+            public void onSkipToNext() {
+                super.onSkipToNext();
+                playNext();
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                super.onSkipToPrevious();
+                playPrevious();
+            }
+        });
+
+        // Create a new Notification
+        final Notification noti = new Notification.Builder(this)
+                // Hide the timestamp
+                .setShowWhen(false)
+                // Set the Notification style
+                .setStyle(new Notification.MediaStyle()
+                        // Attach our MediaSession token
+                        .setMediaSession(mSession.getSessionToken())
+                        // Show our playback controls in the compat view
+                        .setShowActionsInCompactView(0, 1, 2))
+                // Set the Notification color
+                .setColor(0xFFDB4437)
+                // Set the large and small icons
+                .setSmallIcon(R.drawable.oval_shape)
+                // Set Notification content information
+                .setContentText(songs.get(songPosition).getArtist())
+                .setContentInfo(songs.get(songPosition).getAlbum())
+                .setContentTitle(songs.get(songPosition).getTitle())
+                // Add some playback controls
+                .addAction(R.drawable.ic_skip_previous_black_24dp, "prev", retreivePlaybackAction(3))
+                .addAction(R.drawable.ic_pause_black_24dp, "pause", retreivePlaybackAction(1))
+                .addAction(R.drawable.ic_skip_next_black_24dp, "next", retreivePlaybackAction(2))
+                .build();
+
+        // Do something with your TransportControls
+        final MediaController.TransportControls controls = mSession.getController().getTransportControls();
+
+        if (getSystemService(NOTIFICATION_SERVICE) != null) {
+            //noinspection ConstantConditions
+            ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(1, noti);
+        }
     }
 }
