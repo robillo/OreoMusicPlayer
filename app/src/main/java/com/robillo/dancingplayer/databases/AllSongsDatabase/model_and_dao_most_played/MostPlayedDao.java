163e74dc74897a1cc36ccaf1184b6de75046b4d2
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-06-29 15:59:00 +0530

    Most played songs dao and repo fully implemented

--- app/src/main/java/com/robillo/dancingplayer/databases/AllSongsDatabase/model_and_dao_most_played/MostPlayedDao.java
+++ app/src/main/java/com/robillo/dancingplayer/databases/AllSongsDatabase/model_and_dao_most_played/MostPlayedDao.java
@@ -1,8 +1,28 @@
 package com.robillo.dancingplayer.databases.AllSongsDatabase.model_and_dao_most_played;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.robillo.dancingplayer.models.MostPlayed;
+import com.robillo.dancingplayer.models.Song;
+
+import java.util.List;
 
 @Dao
 public interface MostPlayedDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMostPlayed(MostPlayed... mostPlayed);
+
+    @Query("SELECT playCount FROM MostPlayed WHERE songId = :songId")
+    LiveData<Integer> checkIfSongExists(String songId);
+
+    @Query("UPDATE MostPlayed SET playCount = :playCount WHERE songId = :songId")
+    void updateMostPlayed(String songId, int playCount);
+
+    @Query("SELECT Song.* FROM Song INNER JOIN MostPlayed WHERE Song.id = MostPlayed.songId ORDER BY playCount DESC LIMIT :limit")
+    LiveData<List<Song>> getMostPlayedSongs(int limit);
 }
--- app/src/main/java/com/robillo/dancingplayer/databases/AllSongsDatabase/model_and_dao_most_played/MostPlayedRepository.java
+++ app/src/main/java/com/robillo/dancingplayer/databases/AllSongsDatabase/model_and_dao_most_played/MostPlayedRepository.java
@@ -1,10 +1,75 @@
 package com.robillo.dancingplayer.databases.AllSongsDatabase.model_and_dao_most_played;
 
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.robillo.dancingplayer.databases.AllSongsDatabase.model_and_dao_playlists.PlaylistDao;
+import com.robillo.dancingplayer.models.MostPlayed;
+import com.robillo.dancingplayer.models.Song;
+import com.robillo.dancingplayer.views.activities.main.MainActivity;
+
+import java.util.List;
+
 public class MostPlayedRepository {
 
     private MostPlayedDao mostPlayedDao;
 
     public MostPlayedRepository(MostPlayedDao mostPlayedDao) {
         this.mostPlayedDao = mostPlayedDao;
+    }
+
+    void checkIfExistsAndInsert(MainActivity activity, String songId) {
+        LiveData<Integer> integerLiveData = mostPlayedDao.checkIfSongExists(songId);
+
+        integerLiveData.observe(activity, integer -> {
+            if(integer != null) {
+                updateMostPlayedCount(songId, integer + 1);
+            }
+            else {
+                insertMostPlayed(new MostPlayed(songId, 1));
+            }
+        });
+    }
+
+    private void updateMostPlayedCount(String songId, int playCount) {
+        new updateMpAsyncTask(mostPlayedDao).execute(songId, String.valueOf(playCount));
+    }
+
+    private void insertMostPlayed(MostPlayed mostPlayed) {
+        new insertMpAsyncTask(mostPlayedDao).execute(mostPlayed);
+    }
+
+    private LiveData<List<Song>> getMostPlayedSongs(int limit) {
+        return mostPlayedDao.getMostPlayedSongs(limit);
+    }
+
+    static class insertMpAsyncTask extends AsyncTask<MostPlayed, Void, Void> {
+
+        private MostPlayedDao mAsyncTaskDao;
+
+        insertMpAsyncTask(MostPlayedDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(MostPlayed... mostPlayeds) {
+            mAsyncTaskDao.insertMostPlayed(mostPlayeds);
+            return null;
+        }
+    }
+
+    static class updateMpAsyncTask extends AsyncTask<String, Void, Void> {
+
+        private MostPlayedDao mAsyncTaskDao;
+
+        updateMpAsyncTask(MostPlayedDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            mAsyncTaskDao.updateMostPlayed(strings[0], Integer.valueOf(strings[1]));
+            return null;
+        }
     }
 }
